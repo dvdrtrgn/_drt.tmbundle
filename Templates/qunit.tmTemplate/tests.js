2782f763d75889f6b3edb2ef7248775b7425f2a2
dvdrtrgn <dvdrtrgn@gmail.com>
2011-12-11 16:49:01 -0600

    rename templages

---
+++ Templates/qunit.tmTemplate/tests.js
@@ -0,0 +1,17 @@
+/* -VERSION-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- *
+ *  :test/${TM_NEW_FILE_BASENAME}  ^  dvdrtrgn  ^  2011-12-10 .. ${TM_DATE}
+ * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+jQuery(function(){
+    jQuery('body').append('<h1 id="qunit-header">'+document.title+'</h1>'   +
+        '<h2 id="qunit-banner"></h2><div id="qunit-testrunner-toolbar">'    +
+        '<a target="_blank" href="http://docs.jquery.com/QUnit">Docs</a>'   +
+        ' </div><h2 id="qunit-userAgent"></h2><ol id="qunit-tests"></ol>'   +
+        '<div id="qunit-fixture"></div>');
+});
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+// Simple Little Unit Test
+//    module("SLUT");
+//    test('empty',function(){
+//        equal(2+2, 4, 'duh 2+2');
+//    });
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
